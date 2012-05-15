c40e7db393bae7e89fcb410547b9d806fb57a671
shyam mohan kanojia <shyammohankanojia@gmail.com>
2012-05-16 00:56:37 +0530

    heroku env fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,5 @@
 #puts 'SETTING UP DEFAULT USER LOGIN'
 #user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please', :confirmed_at => DateTime.now
 #puts 'New user created: ' << user.name
-puts "New list data"
 
-  List.create(:body =>"A madman/A destructively harmful person")
-  List.create(:body =>"Love of nature")
-  List.create(:body =>"A pile of trash/A rubbish heap")
-  List.create(:body =>"A technical improvement")
-  List.create(:body =>"A devoted scientist")
-  List.create(:body =>"Torture a person/Sadism")
-  List.create(:body =>"Burn a witch at the stake/Religious fanaticism")
-  List.create(:body =>"A mathematical genius")
-  List.create(:body =>"Nonsense/Absurdity/Stupidity")
-  List.create(:body =>"An electrical short circuit/A system breakdown")
-  List.create(:body =>"A uniform")
-  List.create(:body =>"A baby")
-  List.create(:body =>"A good meal")
-  List.create(:body =>"By this ring, I thee wed/Marriage vows")
-  List.create(:body =>"A traffic ticket/A fine")
-  List.create(:body =>"An assembly line/Highly routine labor")
-  List.create(:body =>"Slavery/Being a slave/Being enslaved")
-  List.create(:body =>"Blow up an airliner in flight/An act of terror")
 
