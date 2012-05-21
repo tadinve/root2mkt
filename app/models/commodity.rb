735b1fa51ecb812f0f88bcbcbe1d86644ea720a8
OzoneItSolutions <arun@ozoneitsolutions.com>
2012-05-22 04:49:30 +0530

    geocoder added

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -1,5 +1,5 @@
 class Commodity < ActiveRecord::Base
-	geocoded_by :latitude, :longitude #:full_street_address  #, :latitude  => :lat1, :longitude => :lat2 # ActiveRecord
+	geocoded_by :address  #, :latitude  => :lat1, :longitude => :lat2 # ActiveRecord
 	#reverse_geocoded_by :lat1, :lat2, :address => :location
 	after_validation :geocode
 	reverse_geocoded_by :latitude, :longitude
