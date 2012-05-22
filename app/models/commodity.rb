249045722186644b0dea5cf6ba8abad2cd56d3bf
OzoneItSolutions <arun@ozoneitsolutions.com>
2012-05-22 10:34:09 +0530

    xls loaded

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -1,7 +1,7 @@
 class Commodity < ActiveRecord::Base
-	geocoded_by :address  #, :latitude  => :lat1, :longitude => :lat2 # ActiveRecord
+	#geocoded_by :address  #, :latitude  => :lat1, :longitude => :lat2 # ActiveRecord
 	#reverse_geocoded_by :lat1, :lat2, :address => :location
-	after_validation :geocode
-	reverse_geocoded_by :latitude, :longitude
-	after_validation :reverse_geocode  # auto-fetch address
+	#after_validation :geocode
+	#reverse_geocoded_by :latitude, :longitude
+	#after_validation :reverse_geocode  # auto-fetch address
 end
